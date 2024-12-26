9e102c14e67f6c1223913b19f7202aa4fe1ab532
Augusto Inacio <153130112+Od0g@users.noreply.github.com>
2024-12-26 22:50:48 +0000

    Adiciona afrquivos do dia.

---
+++ HelloWorld copy.java
@@ -0,0 +1,12 @@
+/**
+ * Programa que imprime na tela Olá Mnundo!
+ * @author Od0g
+ */
+
+public class HelloWorld {
+    /*Método principal da classe*/
+    public static void main (String[] args){
+        System.out.println("Olá Mundo!");
+        System.out.println("Meu Primeiro Programa em Java");
+    }//Fim do Método
+}//fim da classe
---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Programa que imprime na tela Olá Mnundo!
+ * @author Od0g
+ */
+
+class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Olá Mundo!");
+    }
+}
